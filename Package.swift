8900488ccef7fdc57f056df0f677aa4b4cee7dcd
Christian Selig <me@christianselig.com>
2025-10-16 12:14:06 -0300

    Package type no longer dynamic (now automatic/static)

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TinyStorage",
-            type: .dynamic,
             targets: ["TinyStorage"]),
     ],
     dependencies: [],
